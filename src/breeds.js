f95bd4ce3e2bc81f0639002f6fb87dd355c75805
juuut29-png <juuut29@gmail.com>
2025-10-16 14:01:52 +0200

    Create breeds.js

---
+++ src/breeds.js
@@ -0,0 +1,51 @@
+// ================================
+// 🐾 VetZen — Razas de animales
+// ================================
+
+export const BREEDS = {
+  Perro: [
+    'Afgano', 'Airedale Terrier', 'Akita Inu', 'Alaskan Malamute', 'American Bully', 
+    'American Staffordshire', , 'Beagle' , 'Bearded Collie', 'Bichón Frise', 'Bichón Habanero', 
+    'Bichón Maltes', 'Biewer terrier', 'Bobtail, Border Collie', 'Bóxer', 'Boyero De Berna', 'Braco Alemán', 
+    'Braco de Weimar', 'Braco Húngaro', 'Bretón', 'Boston Terrier', 'Bulldog Francés', 'Bulldog Inglés', 
+    'Bullmastiff', 'Bull Terrier', 'Caniche Gigante Blanco', 'Caniche Gigante Color', 'Caniche Mediano Blanco', 
+    'Caniche Mediano Colo',' 'Caniche Toy o Enano Blanco', 'Caniche Toy o Enano Color', 'Carlino', 'Cavalier king Charles Spaniel', 
+    'Chihuahua', 'Chow Chow', Cocker Americano', 'Cocker Inglés', 'Crestado Chino', 'Dalmata', 'Doberman', 'Dogo Aleman', 'Dogo Argentino', 
+    'Dogo de Burdeos', 'Fox Terrier Pelo Corto', 'Fox Terrier Pelo Duro', 'Galgo Español', 'Galgo Italiano', 'Golden Retriever', 'Grifón De Bruselas', 'Husky Siberiano', 
+    'Jack Russell Pelo Duro', 'Jack Russell Pelo Corto', 'Kerry blue terrier', 'Labrador Retriever', 'Labradoodle', 'Maltipoo', 'Mastin', 'Papillon', 'Pastor Alemán',
+    'Pastor Australiano', 'Pastor Belga Malinois', 'Pastor De Brie', 'Perro De Agua  Español', 'Pinscher', 'Podenco', 'Pomerania', 'Pointer, Samoyedo', 
+    'San Bernardo' , 'Schnauzer Miniatura', 'Schnauzer Mediano', 'Schnauzer Gigante', 'Scottish terrier', 'Setter Inglés', 'Shiba Inu', 
+    'Shih Tzu', 'Teckel pelo corto', 'Terranova', 'Welsh Corgi', 'West Highland White Terrier', 'Yorkshire', 'Cruze pequeño', 'Cruze mediano', 'Cruze grande'
+  ],
+
+  Gato: [
+    'Europeo','Siamés','Persa','Maine Coon','Bengalí','Sphynx','British Shorthair',
+    'Ragdoll','Abisinio','Oriental','Exótico de Pelo Corto','Azul Ruso',
+    'Bosque de Noruega','Chartreux','Himalayo','Cornish Rex','Birmano','Bombay'
+  ],
+
+  Reptil: [
+    'Gecko Leopardo','Camaleón','Iguana Verde','Serpiente del Maíz','Dragón Barbudo',
+    'Pitón Real','Tortuga Rusa','Tortuga de Orejas Rojas','Boa Constrictor'
+  ],
+
+  Pez: [
+    'Betta Splendens','Guppy','Goldfish','Cíclido Africano','Cíclido Enano',
+    'Neón Tetra','Molly','Platy','Pez Ángel','Barbo Tigre'
+  ],
+
+  Ave: [
+    'Periquito','Canario','Agapornis','Cacatúa Ninfa','Loro Amazonas',
+    'Guacamayo','Diamante Mandarín','Tórtola','Pinzón','Lorito del Amor'
+  ],
+
+  Conejo: [
+    'Belier','Enano Holandés','Mini Rex','Cabeza de León','Gigante de Flandes',
+    'Chinchilla','Angora Inglés','Tan','California'
+  ],
+
+  Roedor: [
+    'Hamster Dorado','Hamster Enano','Cobaya','Chinchilla','Gerbo','Rata Doméstica',
+    'Ratón Doméstico','Degú','Pezón','Ardilla de Corea'
+  ]
+}
